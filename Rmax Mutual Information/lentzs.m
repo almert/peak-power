4db3c1f8117040ec6a993330f37b7bd0dd9df59a
Mert Al <32966694+almert@users.noreply.github.com>
2018-08-18 18:23:49 -0400

    Add files via upload

---
+++ Rmax Mutual Information/lentzs.m
@@ -0,0 +1,34 @@
+function [out] = lentzs(nu,z)
+% Y = steeds(nu,z) implements Lentz's method for computing the continued
+% fraction, which gives the ratio of two modified Bessel functions
+% evaluated at z. The output is I_{nu}(z)/I_{nu-1}(z)
+% Here, nu is the dimension, z is the input of the Bessel functions.
+
+eps = 1e-10;
+tiny = 1e-30;
+fn = tiny;
+Cn = fn;
+Dn = 0;
+
+inc = 2/z;
+bn = (2*nu-2)/z;
+while true
+    bn = bn+inc;
+    Dn = bn+Dn;
+    if abs(Dn)<tiny
+        Dn = tiny;
+    end
+    Cn = bn+1/Cn;
+    if abs(Cn)<tiny
+        Cn = tiny;
+    end
+    Dn = 1/Dn;
+    df = Cn*Dn;
+    fn = fn*df;
+    if abs(df-1)<eps
+        break;
+    end
+end
+    out = fn;
+end
+
---
+++ Rmax Mutual Information/steeds.m
@@ -0,0 +1,25 @@
+function [out] = steeds(nu,z)
+% Y = steeds(nu,z) implements Steed's method for computing the continued 
+% fraction, which gives the ratio of two modified Bessel functions 
+% evaluated at z. The output is I_{nu}(z)/I_{nu-1}(z)
+% Here, nu is the dimension, z is the input of the Bessel functions.
+
+    eps = 1e-10;
+    Dn = z/(2*nu);
+    dCn = Dn;
+    Cn = Dn;
+    
+    inc = 2/z;
+    bn = 2*nu/z;
+    while true
+        bn = bn+inc;
+        Dn = 1/(Dn+bn);
+        dCn = (bn*Dn-1)*dCn;
+        Cn = Cn+dCn;
+        if abs(dCn) < eps
+            break;
+        end
+    end
+    out = Cn;  
+end
+
