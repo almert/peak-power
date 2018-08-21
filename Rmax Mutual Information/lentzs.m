672f14e29ae2ff52c2a9d20f930cd260e4cfb9a6
Mert Al <32966694+almert@users.noreply.github.com>
2018-08-21 16:40:36 -0400

    Update lentzs.m

--- Rmax Mutual Information/lentzs.m
+++ Rmax Mutual Information/lentzs.m
@@ -4,8 +4,8 @@ function [out] = lentzs(nu,z)
 % evaluated at z. The output is I_{nu}(z)/I_{nu-1}(z)
 % Here, nu is the dimension, z is the input of the Bessel functions.
 
-if z == 0
-    out = 0;
+if abs(z) <= 1e-30
+    out = z;
     return;
 end
 
